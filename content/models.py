f0f9ce9ded97ef40464617e4b29a55ed596071ba
Christina Nkya <christina.nkya@sdgku.edu>
2024-09-24 21:38:17 -0400

    Class 2 update

--- content/models.py
+++ content/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    year = models.IntegerField()
+    image = models.ImageField(upload_to='static/img')
+    repository = models.URLField()
+    skills = models.ManyToManyField(Skill)
+
+
