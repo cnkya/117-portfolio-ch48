8a1c72cb396eec64e07dea24c75581bc3d03a108
Christina Nkya <christina.nkya@sdgku.edu>
2024-09-28 14:35:12 -0400

    class 4

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -7,3 +9,35 @@ def home_view(request):
 
 def about_view(request):
     return render(request, 'pages/about.html')
+
+def contact_view(request):
+    if request.method == "POST":
+        #send message
+        
+
+        form = ContactForm(request.POST) #to validate the information
+
+        if form.is_valid():
+            print("Sending")
+
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            email_message = f"Name:{name}\nEmail:{email}\nMessage:\n{message}"
+
+            send_mail(
+                "Email from" + name, 
+                message,
+                email,
+                ["christina.nkya@sdgku.edu"]
+
+            )
+
+        else:
+            print("Invalid Form")
+    else:
+        #display form
+        form = ContactForm()
+
+    return render(request, 'pages/contact.html', {'form': form})
